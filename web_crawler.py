541d9cc7a1acea69ba69334182657fafee9ca6cf
Kristen <kristen@Kristens-MacBook-Air.local>
2023-06-24 07:27:42 -0700

    Fixed logging to file

--- web_crawler.py
+++ web_crawler.py
@@ -11,24 +11,39 @@ import csv
 from datetime import datetime
 import os
 import re
+import logging
 
 class KeyWordSpider(scrapy.Spider):
     name = "abortion_search"
     keyword = "abortion"
     inputFile = "./data/schools.csv"
     outputDir = "./output/"
-    outputFile = None
+    outputFileReport = None
+    outputFileLog = None
 
     def start_requests(self):
-        if not os.path.isfile(self.inputFile):
-            self.log(f'Input File Not Found: {self.inputFile}')
 
+        # Configure Output Path
         if not os.path.exists(self.outputDir):
             os.makedirs(self.outputDir)     
 
+        self.outputFileReport = "./output/report_" +  datetime.now().strftime("%Y%m%d%H%M%S") + ".csv"
+        self.outputFileLog = "./output/report_" +  datetime.now().strftime("%Y%m%d%H%M%S") + ".log"
 
-        self.outputFile = "./output/report_" +  datetime.now().strftime("%Y%m%d%H%M%S") + ".csv"
-        self.log(f"***Output File:{self.outputFile}")
+        # Configure logging
+        logging.basicConfig(filename=self.outputFileLog, 
+                            filemode='a',
+                            format="%(levelname)s: %(message)s", 
+                            level=logging.DEBUG,
+                            force=True
+        )
+
+        # Parse Input
+        if not os.path.isfile(self.inputFile):
+            logging.error(f'Input File Not Found: {self.inputFile}')
+
+
+        logging.info(f"Output File Report:{self.outputFileReport}")
 
         with open(self.inputFile , newline='\n') as csvfile:
             inputFileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -45,15 +60,15 @@ class KeyWordSpider(scrapy.Spider):
                     request.cb_kwargs['domain'] = domain
                     yield request
                 else:
-                    print("Invalid Row: " + ', '.join(row))
+                    logging.error("Invalid Row: " + ', '.join(row))
 
 
     def parse(self, response, school, domain):
 
-        print(f'Parsing {response.url}')
+        logging.info(f'Parsing {response.url}')
         
         # Log the keywords on this page
-        file = open(self.outputFile, "w")
+        file = open(self.outputFileReport, "w")
         try:
             searchResults = response.xpath("//*[contains(text(), '{keyword}')]").getall()
             for searchResult in searchResults:
@@ -67,7 +82,7 @@ class KeyWordSpider(scrapy.Spider):
         # Look for links to follow
         links = response.xpath('//div/p/a')
         numLinks = len(links)
-        print(f'**LINKS: {numLinks}')
+        logging.info(f'LINKS: {numLinks}')
         for link in links:
             if re.search(rootUrl, link, re.IGNORECASE):
                     request2 = scrapy.Request(url=url, callback=self.parse)
@@ -78,6 +93,9 @@ class KeyWordSpider(scrapy.Spider):
 
 
 def main():
+
+
+    # Start the Crawler
     settings = {
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     }
